f52131ca30ca266f8350d7ba120e2f5c7c263beb
Igor Beliakovski <nuevo-design@mail.ru>
2016-09-21 13:40:39 +0300

    Индекс почти, в трех вьюпортах

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
-var logoNoJs = document.querySelector('.main-nav__logo--nojs');
+var pHeaderNoJs = document.querySelector('.page-header--nojs');
+var mainNavNoJs = document.querySelector('.main-nav--nojs');
+var mainNavLogoNoJs = document.querySelector('.main-nav__logo--nojs');
 var navListNoJs = document.querySelector('.main-nav__list--nojs');
 var navToggleOpen = document.querySelector('.main-nav__toggle--open');
 var navToggleClosed = document.querySelector('.main-nav__toggle--closed');
@@ -7,29 +9,42 @@ var menuToggler = document.querySelector('.main-nav__toggle');
 
 window.addEventListener('DOMContentLoaded', function(event) {
 
-    if(logoNoJs.classList.contains('main-nav__logo--nojs')) {
-      logoNoJs.classList.remove('main-nav__logo--nojs');
+    if(mainNavNoJs.classList.contains('main-nav--nojs')) {
+      mainNavNoJs.classList.remove('main-nav--nojs');
+    }
+    if(pHeaderNoJs.classList.contains('page-header--nojs')) {
+      pHeaderNoJs.classList.remove('page-header--nojs');
+    }
+    if(mainNavLogoNoJs.classList.contains('main-nav__logo--nojs')) {
+      mainNavLogoNoJs.classList.remove('main-nav__logo--nojs');
     }
     if(navListNoJs.classList.contains('main-nav__list--nojs')) {
       navListNoJs.classList.remove('main-nav__list--nojs');
     }
     if(navToggleOpen.classList.contains('main-nav__toggle--open')) {
       navToggleOpen.classList.remove('main-nav__toggle--open');
-      //navToggleClosed.classList.add('main-nav__toggle--closed');
     }
 } );
 
 menuToggler.addEventListener("click", function(event) {
   event.preventDefault();
-  logoNoJs.classList.toggle('main-nav__logo--nojs');
+  mainNavNoJs.classList.toggle('main-nav--nojs');
+  pHeaderNoJs.classList.toggle('page-header--nojs');
+  mainNavLogoNoJs.classList.toggle('main-nav__logo--nojs');
   navListNoJs.classList.toggle('main-nav__list--nojs');
   navToggleOpen.classList.toggle('main-nav__toggle--open');
 });
 
 window.addEventListener('keydown', function(event) {
   if(event.keyCode == 27) {
-    if(logoNoJs.classList.contains('main-nav__logo--nojs')) {
-      logoNoJs.classList.remove('main-nav__logo--nojs');
+    if(pHeaderNoJs.classList.contains('page-header--nojs')) {
+      pHeaderNoJs.classList.remove('page-header--nojs');
+    }
+    if(mainNavNoJs.classList.contains('main-nav--nojs')) {
+      mainNavNoJs.classList.remove('main-nav--nojs');
+    }
+    if(mainNavLogoNoJs.classList.contains('main-nav__logo--nojs')) {
+      mainNavLogoNoJs.classList.remove('main-nav__logo--nojs');
     }
     if(navListNoJs.classList.contains('main-nav__list--nojs')) {
       navListNoJs.classList.remove('main-nav__list--nojs');
